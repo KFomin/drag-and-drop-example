b58fbc33646d7e95a5f2fc2e10343763eba4d229
KFomin <fominkonstantiin@gmail.com>
2025-02-09 16:34:09 +0200

    simplify dragndrop logic

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,12 @@
 import {Component} from '@angular/core';
 import {MatDrawer, MatDrawerContainer, MatDrawerContent} from "@angular/material/sidenav";
 import {NgForOf, NgStyle} from "@angular/common";
-import {DragDropModule} from "@angular/cdk/drag-drop";
+import {CdkDrag, CdkDragDrop, CdkDragEnd, CdkDragRelease, DragDropModule, Point} from "@angular/cdk/drag-drop";
 
 interface Shape {
   id: number;
   type: string;
-  x: number;
-  y: number;
+  position: Point;
   text: string;
 }
 
@@ -45,17 +44,19 @@ export class HomeComponent {
     const type = event.dataTransfer?.getData('text/plain');
 
     if (type) {
-      const canvasRect = (event.target as HTMLElement).getBoundingClientRect();
-      const x = event.clientX - canvasRect.left;
-      const y = event.clientY - canvasRect.top;
+      const x = event.layerX;
+      const y = event.layerY;
+      const position = this.toPoint(x, y);
+
+      console.log(position);
 
       const newShape: Shape = {
-        id: this.nextId++,
-        type,
-        x,
-        y,
-        text: ''
-      };
+          id: this.nextId++,
+          type,
+          position,
+          text: ''
+        }
+      ;
 
       this.shapes.push(newShape);
     }
@@ -63,5 +64,25 @@ export class HomeComponent {
 
   onDragStart(event: DragEvent, type: string) {
     event.dataTransfer?.setData('text/plain', type); // Передаем тип формы
+  }
+
+  toPoint(x: number, y: number): Point {
+    return {x: x, y: y};
+  }
+
+  onDragEnd($event: CdkDragEnd, id: number) {
+    console.log($event);
+    // this.shapes.map(shape => {
+    //     if (shape.id === id) {
+    //       console.log($event.distance.x);
+    //       console.log($event.dropPoint.x);
+    //       shape.position = {
+    //         x: shape.position.x  + $event.distance.x,
+    //         y: shape.position.y + $event.distance.y
+    //       };
+    //       console.log(shape.position.x);
+    //     }
+    //   }
+    // )
   }
 }
